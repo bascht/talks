1de3fe9ba264673ddf8b591b8702ae3cb8aa8443
Sebastian Schulze <github.com@bascht.com>
2016-06-08 18:11:12 +0200

    Add RubyShift 2016-06

---
+++ 2016-06-munich-rubyshift/slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: 'News Bulletin', 
+     subtitle: 'Rubyshift June 2016', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: true, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'images/org-icon.png', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: '@bascht', 
+     company: '', 
+     gplus: '', 
+     twitter: 'bascht', 
+     www: 'https://bascht.com', 
+     github: 'http://github.com/bascht', 
+   }]
+};
